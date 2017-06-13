51a0eec56a8147e4be084282bb93d15ea7274034
Androbin <robin.richtsfeld@gmail.com>
2017-06-14 01:36:58 +0200

    Update EntityRenderer

--- src/de/androbin/rpg/gfx/EntityRenderer.java
+++ src/de/androbin/rpg/gfx/EntityRenderer.java
@@ -6,33 +6,33 @@ import java.awt.geom.*;
 import java.awt.image.*;
 import de.androbin.gfx.util.*;
 import de.androbin.rpg.*;
+import de.androbin.util.txt.*;
 
 public class EntityRenderer extends Renderer {
-  public final BufferedImage[][] images;
-  public final Entity entity;
+  protected final Entity entity;
+  protected final BufferedImage[][] animation;
   
-  public EntityRenderer( final BufferedImage[][] images, final Entity entity ) {
-    this.images = images;
+  public EntityRenderer( final Entity entity, final BufferedImage[][] animation ) {
     this.entity = entity;
+    this.animation = animation;
   }
   
   public static BufferedImage[][] createSheet( final String path, final int res ) {
-    final int nDir = Direction.values().length;
-    final BufferedImage[][] images = new BufferedImage[ nDir ][];
+    final Direction[] directions = Direction.values();
+    final BufferedImage[][] animation = new BufferedImage[ directions.length ][];
     
-    for ( int iDir = 0; iDir < images.length; iDir++ ) {
-      final String dir = Direction.values()[ iDir ].name();
+    for ( int i = 0; i < animation.length; i++ ) {
+      final String dir = CaseUtil.toProperCase( directions[ i ].name() );
       final BufferedImage image = ImageUtil.loadImage( path + "/" + dir + ".png" );
       
-      final int steps = image.getWidth() / res;
-      images[ iDir ] = new BufferedImage[ steps ];
+      animation[ i ] = new BufferedImage[ image.getWidth() / res ];
       
-      for ( int step = 0; step < steps; step++ ) {
-        images[ iDir ][ step ] = image.getSubimage( step * res, 0, res, res );
+      for ( int j = 0; j < animation[ i ].length; j++ ) {
+        animation[ i ][ j ] = image.getSubimage( j * res, 0, res, res );
       }
     }
     
-    return images;
+    return animation;
   }
   
   @ Override
@@ -42,13 +42,8 @@ public class EntityRenderer extends Renderer {
   
   @ Override
   public void render( final Graphics2D g ) {
-    final int ordinal = entity.viewDir.ordinal();
-    final float moveProgress = entity.getMoveProgress();
-    
-    final BufferedImage[] direction = images[ ordinal ];
-    
-    final Rectangle2D.Float bounds = new Rectangle2D.Float( 0f, 0f, scale, scale );
-    
-    drawImage( g, direction[ (int) ( moveProgress * direction.length ) ], bounds );
+    final int i = entity.viewDir.ordinal();
+    final int j = (int) ( entity.getMoveProgress() * animation[ i ].length );
+    drawImage( g, animation[ i ][ j ], 0f, 0f, scale, scale );
   }
 }
